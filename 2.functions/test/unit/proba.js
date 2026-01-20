e2a7515881ab03bc41848b1952ca9f2c94ff5167
Aleksey Kharlanov <harlan14@mail.ru>
2026-01-20 19:25:17 +0700

    DZ complit

---
+++ 2.functions/test/unit/proba.js
@@ -0,0 +1,11 @@
+function sortByName(inputArray) {
+    inputArray.sort(function(a,b) {
+        var nameA = a.toLowerCase(),
+        nameB = b.toLowerCase();
+
+        if (nameA < nameB)
+
+            retur -1;
+        if(nameA < nameB)
+    })
+}
--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,19 @@
 function compareArrays(arr1, arr2) {
+
+    return arr1.length === arr2.length 
+    && arr1.every((item, index) => item === arr2[index]);
   
 }
 
 function getUsersNamesInAgeRange(users, gender) {
+
+    const filteredUsers = users.filter(user => user.gender === gender);
+
+    if (filteredUsers.lenght === 0) {
+        return 0;
+    }
+
+    const totalAge = filteredUsers.reduce((sum, user) => sum + user.age, 0);
+    return totalAge / filteredUsers.length;
   
 }
